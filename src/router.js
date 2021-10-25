f0080256b24ac4f1558e72ff90ef3964a5d8d7d5
Stavros <snikoletopoulos@hotmail.com>
2021-10-26 01:11:35 +0300

    Register route components

--- src/router.js
+++ src/router.js
@@ -1,16 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CoachDetail from "./pages/coaches/CoachDetail.vue";
+import CoachesList from "./pages/coaches/CoachesList.vue";
+import CoachRegistation from "./pages/coaches/CoachRegistation.vue";
+import ContactCoach from "./pages/requests/ContactCoach.vue";
+import RequestsReceived from "./pages/requests/RequestsReceived.vue";
+import NotFound from "./pages/NotFound.vue";
+
 export default createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
-    { path: '/couaches', component: null },
+    { path: '/couaches', component: CoachesList },
     {
       path: '/couaches/:id',
-      component: null,
-      children: [{ path: 'contact', component: null }]
+      component: CoachDetail,
+      children: [{ path: 'contact', component: ContactCoach }]
     },
-    { path: '/register', component: null },
-    { path: '/:notFound(.*)', component: null }
+    { path: '/register', component: CoachRegistation },
+    { path: '/requests', component: RequestsReceived },
+    { path: '/:notFound(.*)', component: NotFound }
   ]
 });
