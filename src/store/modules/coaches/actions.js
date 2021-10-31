3dd36d36a898f5a20af5db176acb0873913a0046
Stavros <snikoletopoulos@hotmail.com>
2021-10-31 16:43:29 +0200

    Add new coach to Vuex
    Create mutations and actions

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,1 +1,14 @@
-export default {};
+export default {
+  registerCoach(context, payload) {
+    const coachData = {
+      id: "c3",
+      firstName: payload.first,
+      lastName: payload.last,
+      description: payload.desc,
+      hourlyRate: payload.rate,
+      areas: payload.areas,
+    };
+
+    context.commit('registerCoach', coachData)
+  },
+};
