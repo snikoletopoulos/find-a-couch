1ccdab532fa80b55411fdc0dd091ad6643bb0c18
Stavros <snikoletopoulos@hotmail.com>
2021-11-01 01:45:25 +0200

    Handle error in UI

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,4 +1,7 @@
 <template>
+  <base-dialog :show="!!error" title="An error occurred." @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <coach-filter @change-filter="setFilters" />
   </section>
@@ -27,6 +30,7 @@
     data() {
       return {
         isLoading: false,
+        error: null,
         activeFilters: {
           frontend: true,
           backend: true,
@@ -63,8 +67,15 @@
       },
       async loadCoaches() {
         this.isLoading = true;
-        await this.$store.dispatch("coaches/loadCoaches");
+        try {
+          await this.$store.dispatch("coaches/loadCoaches");
+        } catch (error) {
+          this.error = error.message || "Something went wrong";
+        }
         this.isLoading = false;
+      },
+      handleError() {
+        this.error = null;
       },
     },
     created() {
