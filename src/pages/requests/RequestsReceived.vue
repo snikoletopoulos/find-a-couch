7afa4f0dc6554e97ee70d1fe8925fe59b9160cfb
Stavros <snikoletopoulos@hotmail.com>
2021-11-01 01:29:21 +0200

    Display a coach current requests
    Create Vuex module for requests

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,9 +1,53 @@
 <template>
-  REQUESTS
+  <section>
+    <base-card>
+      <header>
+        <h2>Requests Received</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <request-item
+          v-for="request in receivedRequests"
+          :key="request.id"
+          :email="request.userEmail"
+          :message="request.message"
+        />
+      </ul>
+      <h3 v-else>You haven't received any requests yet</h3>
+    </base-card>
+  </section>
 </template>
 
 <script>
-  export default {};
+  import RequestItem from "../../components/requests/RequestItem.vue";
+
+  export default {
+    computed: {
+      receivedRequests() {
+        return this.$store.getters["requests/requests"];
+      },
+      hasRequests() {
+        return this.$store.getters["requests/hasRequests"];
+      },
+    },
+    components: {
+      RequestItem,
+    },
+  };
 </script>
 
-<style scoped></style>
+<style scoped>
+  header {
+    text-align: center;
+  }
+
+  ul {
+    list-style: none;
+    margin: 2rem auto;
+    padding: 0;
+    max-width: 30rem;
+  }
+
+  h3 {
+    text-align: center;
+  }
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,20 @@
 import { createStore } from "vuex";
-import coachesModule from './modules/coaches/index';
+import coachesModule from "./modules/coaches/index";
+import requestsModule from "./modules/requests/index";
 
 export default createStore({
   modules: {
-    coaches: coachesModule
+    coaches: coachesModule,
+    requests: requestsModule
+  },
+  state() {
+    return {
+      userId: "c3",
+    };
+  },
+  getters: {
+    userId(store) {
+      return store.userId;
+    },
   },
 });
