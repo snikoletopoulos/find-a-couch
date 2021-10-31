0ab99c8beec811ebb369e16b7aeafe68ea425ef2
Stavros <snikoletopoulos@hotmail.com>
2021-10-31 16:44:06 +0200

    Get form data and register coach

--- src/pages/coaches/CoachRegistation.vue
+++ src/pages/coaches/CoachRegistation.vue
@@ -2,7 +2,7 @@
   <section>
     <base-card>
       <h2>Register as a coach now!</h2>
-      <coach-form />
+      <coach-form @save-data="saveData" />
     </base-card>
   </section>
 </template>
@@ -10,6 +10,12 @@
 <script>
   import CoachForm from "../../components/coaches/CoachForm.vue";
   export default {
+    methods: {
+      saveData(data) {
+        this.$store.dispatch("coaches/registerCoach", data);
+        this.$router.replace("/coaches");
+      },
+    },
     components: {
       CoachForm,
     },
