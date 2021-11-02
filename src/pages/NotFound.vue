181461ed19b1af124959f85de64abc169a6ee33f
Stavros <snikoletopoulos@hotmail.com>
2021-11-02 02:21:58 +0200

    Create NotFound page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,11 +1,17 @@
 <template>
-
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found - maybe check out all our
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
+  </section>
 </template>
 
 <script>
-  export default {
-
-  }
+  export default {};
 </script>
 
 <style scoped></style>
